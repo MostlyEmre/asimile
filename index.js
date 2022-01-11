804fae529de397f3091d10de638a781df0de14b5
Emre <6238977+MostlyEmre@users.noreply.github.com>
2022-01-10 22:48:29 -0500

    fixed replacement bug replace() to replaceAll()

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // replaces Turkish characters with English counterparts.
 const asimile = (string) => {
-  return string.toLowerCase().replace("ı", "i").replace("ö", "o").replace("ü", "u").replace("ç", "c").replace("ğ", "g").replace("ş", "s");
+  return string
+    .toLowerCase()
+    .replaceAll("ı", "i")
+    .replaceAll("ö", "o")
+    .replaceAll("ü", "u")
+    .replaceAll("ç", "c")
+    .replaceAll("ğ", "g")
+    .replaceAll("ş", "s");
 };
 
 module.exports = asimile;
